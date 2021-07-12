92500bc076ddbec796e2bf2693cfb520e6c6d556
Angela Busato <busato.angela@gmail.com>
2021-07-12 16:44:21 +0200

    [add] import button to header compont

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '../Button/Button';
 
 export default function Header() {
     return (
         <div className="header">
-            <h1>This is the header</h1>
+            {/* <h1>This is the header</h1> */}
+            <div className="header__container">
+                <Button text="Import" type="button" />
+            </div>
         </div>
     )
 }
