789edca7b0cd1e2e2e7993c4ebebd34a39d672fb
Angela Busato <busato.angela@gmail.com>
2021-07-12 11:46:21 +0200

    [add] main and header component to App

--- src/features/App/App.js
+++ src/features/App/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from "../../components/Header/Header";
+import Main from '../../components/Main/Main';
 
 export default function App() {
     return (
         <div>
-            <h1>Hello World</h1>
+            {/*<h1 id="app_title">Hello World</h1> */}
+            <Header />
+            <Main />
         </div>
     )
 }
