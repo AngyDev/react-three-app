dd7d440ab734cef1a5822aba9120ce7cc6c1eb6b
Angela Busato <busato.angela@gmail.com>
2021-07-13 15:09:43 +0200

    [add] onClick on button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Button(props) {
     return (
-        <button className="button" type={props.type}>{props.text}</button>
+        <button className="button" type={props.type} onClick={props.onClick}>{props.text}</button>
     )
 }
