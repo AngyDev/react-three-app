e83945c12c2c4c4f899f65ceb23cd6cc5c3c66c5
Angela Busato <busato.angela@gmail.com>
2021-07-13 15:11:02 +0200

    [add] modal in import button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../Button/Button';
+import Modal from "../Modal/Modal";
+import ImportModal from "../Modal/ImportModal/ImportModal";
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => { setIsOpen(true) };
+
     return (
         <div className="header">
             {/* <h1>This is the header</h1> */}
             <div className="header__container">
-                <Button text="Import" type="button" />
+                <Button text="Import" type="button" onClick={handleClick} />
+                <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+                    <ImportModal onClose={() => setIsOpen(false)}/>
+                </Modal>
             </div>
         </div>
     )
