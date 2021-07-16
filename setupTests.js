b6a01ca45defbfafd53b16c8d47fcac9d94e808f
Angela Busato <busato.angela@gmail.com>
2021-07-16 13:29:09 +0200

    [add] global variable for jest error

--- setupTests.js
+++ setupTests.js
@@ -2,3 +2,6 @@ import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
+
+// Error on URL.createObjectURL
+global.URL.createObjectURL = jest.fn();
